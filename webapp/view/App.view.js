e2082e9acaa4fe055ba56bdc6ca804e5dbfa9422
Hector Flores <hecto932@gmail.com>
2016-05-27 19:26:24 -0400

    Week 1 - Unit 3: Creating a Configurable App Component

--- webapp/view/App.view.js
+++ webapp/view/App.view.js
@@ -2,18 +2,26 @@ sap.ui.jsview("opensap.myapp.view.App", {
 
 	/** Specifies the Controller belonging to this View. 
 	 * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-	 * @memberOf controlActivities.view.App
+	 * @memberOf opensap.myapp.view.App
 	 */
 	getControllerName: function() {
-		return null;
+		return "opensap.myapp.controller.App";
 	},
 
 	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
 	 * Since the Controller is given to this method, its event handlers can be attached right away. 
-	 * @memberOf controlActivities.view.App
+	 * @memberOf opensap.myapp.view.App
 	 */
 	createContent: function(oController) {
-		return new sap.m.Carousel({
+
+		var oButton = new sap.m.Button({
+			text: "Say Hello!",
+			press: function(oEvent){
+				oController.onShowHello();
+			}
+		});
+
+		var oCarousel = new sap.m.Carousel({
 			pages: [
 				new sap.m.Image({
 					src: "https://upload.wikimedia.org/wikipedia/commons/9/9f/GEO_Globe.jpg",
@@ -25,6 +33,21 @@ sap.ui.jsview("opensap.myapp.view.App", {
 				})
 			]
 		});
+
+		var oPage = new sap.m.Page({
+			content: [
+				oCarousel,
+				oButton
+			]
+		});
+
+		var App = new sap.m.App({
+			pages: [
+				oPage
+			]
+		})
+
+		return App;
 	}
 
 });
